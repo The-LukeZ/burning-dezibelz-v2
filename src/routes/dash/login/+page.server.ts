8f4c9bfedf0c48397c827c7caaecb2997e40deb5
The-LukeZ <luke.hent3005@gmail.com>
2025-05-20 23:49:04 +0200

    refactor: Remove unused load function for session check in login page

--- src/routes/dash/login/+page.server.ts
+++ src/routes/dash/login/+page.server.ts
@@ -2,14 +2,6 @@ import { PUBLIC_SUPABASE_OAUTH_CALLBACK_URL } from "$env/static/public";
 import { supabaseAdmin } from "$lib/server/supabase.js";
 import { fail, redirect } from "@sveltejs/kit";
 
-export async function load({ locals }) {
-  // Check if logged in, if yes, redirect to /dash
-  const {
-    data: { session },
-    error,
-  } = await supabaseAdmin.auth.getSession();
-}
-
 export const actions = {
   login: async () => {
     const { data, error: err } = await supabaseAdmin.auth.signInWithOAuth({
