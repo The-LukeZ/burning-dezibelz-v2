f43c6bb5500f7c05bf87ec4c37b93aa50aaa2439
The-LukeZ <luke.hent3005@gmail.com>
2025-05-22 20:56:00 +0200

    feat: add generateConcertId function to create unique concert IDs based on date

---
+++ src/lib/server/utils.ts
@@ -0,0 +1,22 @@
+import type { SupabaseClient } from "@supabase/supabase-js";
+import type { Database } from "./supabase";
+
+export async function generateConcertId(
+  isoTimestamp: string,
+  supabase: SupabaseClient<Database>,
+): Promise<string> {
+  // Schema: YYYY-MM-DD-I (I=Index)
+  const dateString = isoTimestamp.split("T")[0];
+
+  // Get amount of concerts on that date
+  const { count, data, error } = await supabase.rpc("count_concerts_on_date", { concert_date: dateString });
+  if (error || count === null) {
+    console.error("Error counting concerts on date:", error);
+    throw new Error("Failed to generate concert ID");
+  }
+
+  console.log("Count of concerts on date:", count, data);
+
+  const index = (count ?? 0) + 1;
+  return dateString + "-" + index;
+}
