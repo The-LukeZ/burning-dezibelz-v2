61878e235bc86a8c03986e543c66dde5275fb5e0
The-LukeZ <luke.hent3005@gmail.com>
2025-05-22 20:04:26 +0200

    feat: Update README and add Supabase types for database schema

---
+++ src/lib/server/supabase.ts
@@ -0,0 +1,264 @@
+export type Json = string | number | boolean | null | { [key: string]: Json | undefined } | Json[];
+
+export type Database = {
+  graphql_public: {
+    Tables: {
+      [_ in never]: never;
+    };
+    Views: {
+      [_ in never]: never;
+    };
+    Functions: {
+      graphql: {
+        Args: {
+          operationName?: string;
+          query?: string;
+          variables?: Json;
+          extensions?: Json;
+        };
+        Returns: Json;
+      };
+    };
+    Enums: {
+      [_ in never]: never;
+    };
+    CompositeTypes: {
+      [_ in never]: never;
+    };
+  };
+  public: {
+    Tables: {
+      allowed_users: {
+        Row: {
+          created_at: string | null;
+          created_by: string | null;
+          email: string;
+          notes: string | null;
+          role: Database["public"]["Enums"]["UserRole"] | null;
+        };
+        Insert: {
+          created_at?: string | null;
+          created_by?: string | null;
+          email: string;
+          notes?: string | null;
+          role?: Database["public"]["Enums"]["UserRole"] | null;
+        };
+        Update: {
+          created_at?: string | null;
+          created_by?: string | null;
+          email?: string;
+          notes?: string | null;
+          role?: Database["public"]["Enums"]["UserRole"] | null;
+        };
+        Relationships: [];
+      };
+      concerts: {
+        Row: {
+          abendkasse: boolean;
+          free: boolean;
+          id: string;
+          name: string;
+          notes: string | null;
+          price: number | null;
+          ticket_url: string | null;
+          timestamp: string;
+          type: Database["public"]["Enums"]["ConcertType"];
+          venue_id: string;
+        };
+        Insert: {
+          abendkasse?: boolean;
+          free?: boolean;
+          id: string;
+          name: string;
+          notes?: string | null;
+          price?: number | null;
+          ticket_url?: string | null;
+          timestamp: string;
+          type?: Database["public"]["Enums"]["ConcertType"];
+          venue_id: string;
+        };
+        Update: {
+          abendkasse?: boolean;
+          free?: boolean;
+          id?: string;
+          name?: string;
+          notes?: string | null;
+          price?: number | null;
+          ticket_url?: string | null;
+          timestamp?: string;
+          type?: Database["public"]["Enums"]["ConcertType"];
+          venue_id?: string;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "concerts_venue_id_fkey";
+            columns: ["venue_id"];
+            isOneToOne: false;
+            referencedRelation: "venues";
+            referencedColumns: ["id"];
+          },
+        ];
+      };
+      venues: {
+        Row: {
+          address: string;
+          city: string;
+          country: string;
+          id: string;
+          name: string;
+          postal_code: string;
+          state: string;
+          url: string;
+        };
+        Insert: {
+          address: string;
+          city: string;
+          country: string;
+          id: string;
+          name: string;
+          postal_code: string;
+          state: string;
+          url: string;
+        };
+        Update: {
+          address?: string;
+          city?: string;
+          country?: string;
+          id?: string;
+          name?: string;
+          postal_code?: string;
+          state?: string;
+          url?: string;
+        };
+        Relationships: [];
+      };
+    };
+    Views: {
+      [_ in never]: never;
+    };
+    Functions: {
+      get_concert_counts_per_venue: {
+        Args: Record<PropertyKey, never>;
+        Returns: {
+          venue_id: string;
+          venue_name: string;
+          concert_count: number;
+        }[];
+      };
+    };
+    Enums: {
+      ConcertType: "public" | "closed";
+      UserRole: "admin" | "editor" | "user";
+    };
+    CompositeTypes: {
+      [_ in never]: never;
+    };
+  };
+};
+
+type DefaultSchema = Database[Extract<keyof Database, "public">];
+
+export type Tables<
+  DefaultSchemaTableNameOrOptions extends
+    | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
+    | { schema: keyof Database },
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof Database;
+  }
+    ? keyof (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+        Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
+    : never = never,
+> = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+      Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R;
+    }
+    ? R
+    : never
+  : DefaultSchemaTableNameOrOptions extends keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
+    ? (DefaultSchema["Tables"] & DefaultSchema["Views"])[DefaultSchemaTableNameOrOptions] extends {
+        Row: infer R;
+      }
+      ? R
+      : never
+    : never;
+
+export type TablesInsert<
+  DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"] | { schema: keyof Database },
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof Database;
+  }
+    ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
+  ? Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I;
+    }
+    ? I
+    : never
+  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
+    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
+        Insert: infer I;
+      }
+      ? I
+      : never
+    : never;
+
+export type TablesUpdate<
+  DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"] | { schema: keyof Database },
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof Database;
+  }
+    ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
+  ? Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U;
+    }
+    ? U
+    : never
+  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
+    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
+        Update: infer U;
+      }
+      ? U
+      : never
+    : never;
+
+export type Enums<
+  DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema["Enums"] | { schema: keyof Database },
+  EnumName extends DefaultSchemaEnumNameOrOptions extends {
+    schema: keyof Database;
+  }
+    ? keyof Database[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"]
+    : never = never,
+> = DefaultSchemaEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema["Enums"]
+    ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
+    : never;
+
+export type CompositeTypes<
+  PublicCompositeTypeNameOrOptions extends keyof DefaultSchema["CompositeTypes"] | { schema: keyof Database },
+  CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
+    schema: keyof Database;
+  }
+    ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
+    : never = never,
+> = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
+  : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema["CompositeTypes"]
+    ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
+    : never;
+
+export const Constants = {
+  graphql_public: {
+    Enums: {},
+  },
+  public: {
+    Enums: {
+      ConcertType: ["public", "closed"],
+      UserRole: ["admin", "editor", "user"],
+    },
+  },
+} as const;
