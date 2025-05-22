8f83bd10ba59dbd13ee5a3ce36df94c130976e41
The-LukeZ <luke.hent3005@gmail.com>
2025-05-22 23:43:07 +0200

    fix: update import path for Database type and refactor PUT method for data validation

--- src/routes/api/concerts/[concert_id]/+server.ts
+++ src/routes/api/concerts/[concert_id]/+server.ts
@@ -1,4 +1,4 @@
-import type { Database } from "$lib/server/supabase.js";
+import type { Database } from "$lib/supabase.js";
 
 export async function GET({ params: { concert_id }, locals: { supabase } }) {
   // Fetch a single concert by ID
@@ -17,16 +17,37 @@ export async function GET({ params: { concert_id }, locals: { supabase } }) {
 export async function PUT({ request, locals: { supabase }, params: { concert_id } }) {
   // Update a concert by replacing the entire object (except the id)
   const body = await request.json();
-  const cleanedData = Object.keys(body).map((key) =>
-    typeof body[key] === "string" ? body[key].trim() : body[key],
-  );
+  const cleanedData = Object.keys(body).reduce((acc, key) => {
+    acc[key] = typeof body[key] === "string" ? body[key].trim() : body[key];
+    return acc;
+  }, {} as any) as Database["public"]["Tables"]["concerts"]["Update"];
 
-  const { name, venue_id, abendkasse, free, notes, price, ticket_url, type, timestamp } =
-    cleanedData as Database["public"]["Tables"]["concerts"]["Update"];
+  // Validate the cleaned data
+  let concertData: Database["public"]["Tables"]["concerts"]["Update"] = {
+    notes: cleanedData.notes,
+    type: cleanedData.type,
+    timestamp: cleanedData.timestamp,
+  };
+
+  // Only provide the fields that are allowed to be updated based on type
+  if (cleanedData.type === "closed") {
+    concertData.type = cleanedData.type;
+    concertData.timestamp = cleanedData.timestamp;
+    concertData.venue_id = null;
+    concertData.ticket_url = null;
+    concertData.price = null;
+    concertData.name = "";
+  } else if (
+    cleanedData.type === "public" &&
+    typeof cleanedData.venue_id === "string" &&
+    cleanedData.venue_id.length > 0
+  ) {
+    concertData.venue_id = cleanedData.venue_id;
+  }
 
   const { data, error } = await supabase
     .from("concerts")
-    .update({ venue_id, type, timestamp, name, notes, price, ticket_url, abendkasse, free })
+    .update(concertData)
     .eq("id", concert_id)
     .select()
     .single();
@@ -39,4 +60,18 @@ export async function PUT({ request, locals: { supabase }, params: { concert_id 
   console.log("Updated concert:", data);
 
   return Response.json(data, { status: 200 });
+}
+
+export async function DELETE({ locals: { supabase }, params: { concert_id } }) {
+  // Delete a concert by ID
+  const { error } = await supabase.from("concerts").delete().eq("id", concert_id);
+
+  if (error) {
+    console.error("Error deleting concert:", error);
+    return Response.json({ error }, { status: 500 });
+  }
+
+  console.log("Deleted concert with ID:", concert_id);
+
+  return new Response(null, { status: 204 });
 }
