83ccda65c8eaa79ee60bace8f6ca9dcaccf36cfb
The-LukeZ <luke.hent3005@gmail.com>
2025-05-22 19:48:05 +0200

    feat: Implement modal component with close button and backdrop click functionality

---
+++ src/lib/components/Modal.svelte
@@ -0,0 +1,111 @@
+<script lang="ts">
+  import XIcon from "$lib/assets/XIcon.svelte";
+  import type { Snippet } from "svelte";
+  import type { ClassValue } from "svelte/elements";
+  import { scale } from "svelte/transition";
+
+  type Props = {
+    children?: Snippet;
+    open?: boolean;
+    onOpenChange?: (open: boolean) => void;
+    /**
+     * Whether to show an "X" button in the top right corner of the modal to close it.
+     */
+    withXButton?: boolean;
+    closeOnBackdropClick?: boolean;
+    /**
+     * Additional classes to apply to the modal box.
+     */
+    class?: ClassValue;
+  };
+
+  let {
+    open = $bindable(false),
+    onOpenChange = $bindable(() => {}),
+    withXButton = true,
+    closeOnBackdropClick = true,
+    class: _class = "",
+    children,
+  }: Props = $props();
+
+  $effect(() => {
+    if (!_class.includes("max-w")) {
+      _class = `${_class} max-w-[32rem]`;
+    }
+  });
+
+  $effect(() => {
+    if (open) {
+      document.body.style.overflow = "hidden";
+      document.body.style.paddingRight = `${window.innerWidth - document.documentElement.clientWidth}px`;
+      document.body.style.pointerEvents = "none";
+    } else {
+      // Prevent flicker when closing (when clicking outside the modal box on mobile could cause a flicker)
+      setTimeout(() => {
+        document.body.style.overflow = "";
+        document.body.style.paddingRight = "";
+        document.body.style.pointerEvents = "";
+      }, 150);
+    }
+  });
+</script>
+
+<!-- svelte-ignore a11y_click_events_have_key_events, a11y_no_static_element_interactions -->
+<div
+  class="fixed inset-0 z-[700] grid place-items-center bg-black/25 backdrop-blur-xs"
+  class:hidden={!open}
+  class:pointer-events-auto={open}
+  onclick={() => {
+    if (closeOnBackdropClick) {
+      open = false;
+      onOpenChange(false);
+    }
+  }}
+>
+  {#if open}
+    <div
+      class="modal-box shadow-xl"
+      class:hidden={!open}
+      transition:scale={{ duration: 150 }}
+      onclick={(e) => e.stopPropagation()}
+    >
+      {#if withXButton}
+        <button
+          class="dy-btn dy-btn-circle dy-btn-ghost x-button"
+          onclick={() => {
+            open = false;
+            onOpenChange(false);
+          }}
+        >
+          <XIcon />
+        </button>
+      {/if}
+      {@render children?.()}
+    </div>
+  {/if}
+</div>
+
+<style>
+  .modal-box {
+    position: relative;
+    grid-column-start: 1;
+    grid-row-start: 1;
+    background-color: var(--color-base-100);
+    box-shadow:
+      var(--tw-inset-shadow), var(--tw-inset-ring-shadow), var(--tw-ring-offset-shadow),
+      var(--tw-ring-shadow), var(--tw-shadow);
+    border-radius: var(--radius-box);
+    padding-inline: calc(var(--spacing) * 8);
+    padding-block: calc(var(--spacing) * 6);
+    max-height: 100dvh;
+    overflow-x: clip;
+    overflow-y: auto;
+    overscroll-behavior: contain;
+  }
+
+  .x-button {
+    position: absolute;
+    top: calc(var(--spacing) * 4);
+    right: calc(var(--spacing) * 4);
+  }
+</style>
