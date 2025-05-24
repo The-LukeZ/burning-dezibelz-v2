b0208999be3c473a24be3d2e2901c54132e5add4
The-LukeZ <luke.hent3005@gmail.com>
2025-05-25 00:40:51 +0200

    feat: add Navbar component for site navigation

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,34 @@
+<div class="dy-navbar fixed top-0 right-0 left-0 z-50 h-16 bg-transparent shadow-sm backdrop-blur-2xl">
+  <div class="mx-auto flex h-full w-full max-w-[1280px] items-center justify-between">
+    <div class="navbar-branding w-full md:w-1/2">
+      <a href="/" class="branding hover:bg-primary/20 rounded px-2 py-1.5 transition hover:text-orange-50">
+        <img src="/favicon.ico" alt="Burning Dezibelz Logo" class="size-10" />
+        <span class="text-2xl font-bold">Burning Dezibelz</span>
+      </a>
+    </div>
+    <div class="dy-navbar-end hidden md:flex">
+      <ul class="dy-menu dy-menu-horizontal gap-1 px-1 text-lg font-semibold">
+        <li><a href="/konzerte">Konzerte</a></li>
+        <li><a href="/ueber-uns">Über uns</a></li>
+        <li><a href="/kontakt">Kontakt</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+<style>
+  .branding {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 0.5rem;
+    width: fit-content;
+  }
+
+  .navbar-branding {
+    display: inline-flex;
+    align-items: center;
+    justify-content: flex-start;
+    gap: 0.5rem;
+  }
+</style>
