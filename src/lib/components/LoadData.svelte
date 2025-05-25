ada367df7f298a41eb40d344ea76e73abb3e8998
The-LukeZ <luke.hent3005@gmail.com>
2025-05-25 02:24:58 +0200

    docs: update comment to clarify usage of LoadData component

--- src/lib/components/LoadData.svelte
+++ src/lib/components/LoadData.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  // Import this in a layout or page component to ensure data is loaded before rendering
   import { eventStore, fetchConcerts, fetchVenues } from "$lib/stores.svelte";
   import { onMount } from "svelte";
 
