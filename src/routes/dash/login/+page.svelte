a3efab1764d3728eeaf34017b67b9ef70702a4f9
The-LukeZ <luke.hent3005@gmail.com>
2025-05-25 14:59:01 +0200

    feat: enhance login layout with improved flex structure and spacing

--- src/routes/dash/login/+page.svelte
+++ src/routes/dash/login/+page.svelte
@@ -16,7 +16,7 @@
 </script>
 
 <!-- Login with Google -->
-<div class="absolute top-0 right-0 bottom-0 left-0 flex items-center justify-center">
+<div class="absolute top-0 right-0 bottom-0 left-0 flex flex-col items-center justify-center gap-4">
   {#if error}
     <div class="dy-alert dy-alert-error w-xs justify-center px-8">
       <div>
