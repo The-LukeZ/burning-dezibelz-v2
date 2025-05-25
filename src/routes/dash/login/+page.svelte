dc6e16bb9eb21563e1c4fe8e9ce0ec5fde3cca28
The-LukeZ <luke.hent3005@gmail.com>
2025-05-25 02:25:59 +0200

    feat: update login form layout for improved accessibility and visual consistency

--- src/routes/dash/login/+page.svelte
+++ src/routes/dash/login/+page.svelte
@@ -15,8 +15,8 @@
   });
 </script>
 
-<!-- Login with Google | Uses @tailwindcss/forms -->
-<div class="flex h-dvh w-dvw flex-col items-center justify-center gap-4">
+<!-- Login with Google -->
+<div class="absolute top-0 right-0 bottom-0 left-0 flex items-center justify-center">
   {#if error}
     <div class="dy-alert dy-alert-error w-xs justify-center px-8">
       <div>
@@ -37,17 +37,16 @@
         height="16"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 512 512"
-        class="size-8"
-        ><g
-          ><path d="m0 0H512V512H0" fill="transparent"></path><path
-            fill="#34a853"
-            d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
-          ></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path
-            fill="#fbbc02"
-            d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
-          ></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g
-        ></svg
+        class="block size-8"
       >
+        <g>
+          <path d="m0 0H512V512H0" fill="transparent"></path>
+          <path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path>
+          <path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path>
+          <path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path>
+          <path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path>
+        </g>
+      </svg>
       Login with Google
     </button>
   </form>
