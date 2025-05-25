a98cd3a9e01ff126b1fde59ff8ca30b4d2e97102
The-LukeZ <luke.hent3005@gmail.com>
2025-05-25 17:28:04 +0200

    fix: remove unused 'depends' parameter from load function

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,7 +1,7 @@
 import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from "$env/static/public";
 import { createBrowserClient, createServerClient, isBrowser } from "@supabase/ssr";
 
-export const load = async ({ fetch, data, depends }) => {
+export const load = async ({ fetch, data }) => {
   const supabase = isBrowser()
     ? createBrowserClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
         global: { fetch },
