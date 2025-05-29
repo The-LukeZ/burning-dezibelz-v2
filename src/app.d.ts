68d6e7027117b2d375d3e63e738fa3126b789360
The-LukeZ <luke.hent3005@gmail.com>
2025-05-30 00:28:03 +0200

    feat: add isAdmin property to Locals and PageData interfaces for UI admin feature display

--- src/app.d.ts
+++ src/app.d.ts
@@ -11,11 +11,22 @@ declare global {
       safeGetSession: () => Promise<{ session: Session | null; user: User | null }>;
       session: Session | null;
       user: User | null;
+      /**
+       * This is true if the user is an admin. This only serves the purpose of dynamically
+       * displaying admin features in the UI and does not enforce any access control.
+       *
+       * This is not a security feature and can be passed to the page data.
+       */
+      isAdmin: boolean;
     }
 
     interface PageData {
       session: Session | null;
       user: User | null;
+      /**
+       * This is true if the user is an admin. Only for UI purposes.
+       */
+      isAdmin: boolean;
       currentISODate: string;
     }
 
@@ -31,6 +42,12 @@ declare global {
   type Song = Database["public"]["Tables"]["songs"]["Row"];
 
   type AllowedUser = Database["public"]["Tables"]["allowed_users"]["Row"];
+
+  type NavItem = {
+    href: string;
+    label: string;
+    requiresAdmin?: boolean;
+  }
 }
 
 export {};
