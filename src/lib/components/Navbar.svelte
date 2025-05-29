d1f48b947d309fdbe9ddb99a13ce14a15eaf2c86
The-LukeZ <luke.hent3005@gmail.com>
2025-05-30 00:28:59 +0200

    feat: enhance Navbar component with dynamic admin link visibility and responsive design

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,36 +1,94 @@
 <script lang="ts">
   import { page } from "$app/state";
   import { isCurrentPage } from "$lib";
+  import ChevronLeft from "$lib/assets/ChevronLeft.svelte";
+  import { eventStore, fetchConcerts, fetchVenues } from "$lib/stores.svelte";
+  import { onMount } from "svelte";
+  import { getItemsForPath } from "$lib/data/navigationData";
+  import { goto } from "$app/navigation";
 
-  const navItems = [
-    { href: "/konzerte", label: "Konzerte" },
-    { href: "/ueber-uns", label: "Über uns" },
-    { href: "/kontakt", label: "Kontakt" },
-  ] as const;
+  let isDashboard = $derived(page.url.pathname.startsWith("/dash"));
+  let navItems = $derived(getItemsForPath(page.url.pathname));
+
+  onMount(async () => {
+    if (eventStore.metadata.concertsLoaded === false) {
+      await fetchConcerts({
+        after: new Date(),
+      });
+    }
+    if (eventStore.metadata.venuesLoaded === false) {
+      await fetchVenues();
+    }
+  });
 </script>
 
-<div
+{#snippet navbarLinks()}
+  {#each navItems as item}
+    <li>
+      <a
+        href={item.href}
+        class="nav-btn"
+        class:active-link={isCurrentPage(item.href, page.url)}
+        class:hidden={item.requiresAdmin && !page.data.isAdmin}
+      >
+        {item.label}
+      </a>
+    </li>
+  {/each}
+  {#if isDashboard}
+    <button
+      class="dy-btn dy-btn-error dy-btn-outline"
+      onclick={() => goto("/dash/logout")}
+      aria-label="Logout"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <path d="m16 17 5-5-5-5" />
+        <path d="M21 12H9" />
+        <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
+      </svg>
+    </button>
+  {/if}
+{/snippet}
+
+<nav
   class="dy-navbar fixed top-0 right-0 left-0 z-50 h-(--navbar-height) bg-black/15 shadow-sm backdrop-blur-2xl"
+  class:hidden={page.url.pathname === "/dash/login"}
 >
   <div class="mx-auto flex h-full w-full max-w-[1280px] items-center justify-between">
-    <div class="navbar-branding md:w-1/2">
-      <a href="/" class="branding hover:bg-primary/20 rounded px-2 py-1.5 transition hover:text-orange-50">
-        <img src="/favicon.ico" alt="Burning Dezibelz Logo" class="size-10" />
-        <span class="text-2xl font-bold">Burning Dezibelz</span>
-      </a>
-    </div>
+    {#if !isDashboard}
+      <div class="navbar-branding md:w-1/2">
+        <a href="/" class="branding hover:bg-primary/20 rounded px-2 py-1.5 transition hover:text-orange-50">
+          <img src="/favicon.ico" alt="Burning Dezibelz Logo" class="size-10" />
+          <span class="text-2xl font-bold">Burning Dezibelz</span>
+        </a>
+      </div>
+    {:else}
+      <div class="dy-navbar-start w-fit">
+        <a class="dy-btn dy-btn-secondary dy-btn-square dy-btn-outline" href="/">
+          <ChevronLeft />
+        </a>
+      </div>
+    {/if}
 
-    <div class="dy-navbar-end dy-dropdown-end w-fit">
-      <ul class="dy-menu dy-menu-horizontal hidden items-center gap-1 px-1 text-lg font-semibold sm:flex">
-        {#each navItems as item}
-          <li>
-            <a href={item.href} class="nav-btn" class:active-link={isCurrentPage(item.href, page.url)}>
-              {item.label}
-            </a>
-          </li>
-        {/each}
+    <div class="dy-navbar-end {isDashboard ? 'w-full' : 'w-fit'}">
+      <ul
+        class="dy-menu dy-menu-horizontal hidden items-center gap-1 px-1 text-lg font-semibold {isDashboard
+          ? 'md:flex'
+          : 'sm:flex'}"
+      >
+        {@render navbarLinks()}
       </ul>
-      <div class="dy-dropdown dy-dropdown-end block sm:hidden">
+      <div class="dy-dropdown dy-dropdown-end {isDashboard ? 'md:hidden' : 'sm:hidden'}">
         <div tabindex="0" role="button" class="dy-btn dy-btn-primary dy-btn-soft dy-btn-square">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -52,20 +110,18 @@
           tabindex="0"
           class="dy-dropdown-content dy-menu dy-menu-xl bg-base-200 rounded-box mobile-menu z-1 mt-3 p-2 drop-shadow-lg"
         >
-          {#each navItems as item}
-            <li>
-              <a href={item.href} class="nav-btn" class:active-link={isCurrentPage(item.href, page.url)}>
-                {item.label}
-              </a>
-            </li>
-          {/each}
+          {@render navbarLinks()}
         </ul>
       </div>
     </div>
   </div>
-</div>
+</nav>
 
 <style>
+  nav {
+    padding-inline: 1rem;
+  }
+
   .branding {
     display: flex;
     flex-direction: row;
@@ -89,7 +145,8 @@
       padding-bottom: 5px;
     }
 
-    a {
+    a,
+    button {
       justify-content: center;
       width: 100%;
     }
