b9c42b7536e78f1651cf0b0cf5421a0ecad034a5
The-LukeZ <luke.hent3005@gmail.com>
2025-05-31 03:46:41 +0200

    feat: add image action buttons for opening and downloading images in gallery

--- src/routes/gallerie/+page.svelte
+++ src/routes/gallerie/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import { page } from "$app/state";
   import { buildImageUrl } from "$lib";
+  import ArrowUpRight from "$lib/assets/ArrowUpRight.svelte";
+    import Download from "$lib/assets/Download.svelte";
   import { images } from "$lib/stores/images";
   import { onMount } from "svelte";
 
@@ -34,12 +36,31 @@
     <p class="text-gray-500">Hier ist's ziemlich leer...</p>
   {:else if !loading && $images.length > 0}
     {#each $images as image}
-      <div class="image-card">
+      <div id={image.id} class="image-card">
         <img
-          src={buildImageUrl(image.filename, { width: 400, height: 300, fit: "contain" })}
+          src={buildImageUrl(image.filename, { width: 400, height: 400, fit: "cover" })}
           alt={image.filename}
           loading="lazy"
         />
+        <div class="card-actions">
+          <p class="p-2 text-sm text-white truncate">{image.filename}</p>
+          <a
+            class="dy-btn dy-btn-primary dy-btn-dash dy-btn-sm w-28"
+            href={buildImageUrl(image.filename)}
+            target="_blank"
+          >
+            Open
+            <ArrowUpRight class="size-5" />
+          </a>
+          <a
+            class="dy-btn dy-btn-primary dy-btn-dash dy-btn-sm w-28"
+            href={buildImageUrl(image.filename, { download: true })}
+            target="_blank"
+          >
+            Download
+            <Download class="size-5" />
+          </a>
+        </div>
       </div>
     {/each}
   {/if}
@@ -52,11 +73,42 @@
     gap: 1rem;
     margin-top: 2rem;
     padding: 1rem;
+    justify-content: center;
+    align-items: center;
   }
 
   .image-card {
-    flex: 1 1 calc(33.333% - 1rem);
+    position: relative;
+    flex: 0 1 calc(33.333% - 1rem);
     border-radius: 0.5rem;
     overflow: hidden;
+
+    .card-actions {
+      position: absolute;
+      top: 0;
+      right: 0;
+      left: 0;
+      bottom: 0;
+      display: flex;
+      flex-direction: column;
+      gap: 0.5rem;
+      justify-content: center;
+      align-items: center;
+      opacity: 0;
+      transition: opacity 100ms ease-in-out;
+      background-color: rgba(0, 0, 0, 0.5);
+      overflow: clip;
+
+      p {
+        text-align: center;
+        text-wrap: balance;
+        width: 100%;
+      }
+
+      &:hover {
+        opacity: 1;
+        background-color: rgba(0, 0, 0, 0.75);
+      }
+    }
   }
 </style>
