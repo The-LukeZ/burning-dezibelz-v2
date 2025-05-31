6abb149fae6febe22f45392945fc8529f5dd3cae
The-LukeZ <luke.hent3005@gmail.com>
2025-05-31 17:42:27 +0200

    feat: simplify generateConcertId function by removing venue-related parameters and updating return format

--- src/lib/server/utils.ts
+++ src/lib/server/utils.ts
@@ -1,27 +1,18 @@
 import type { SupabaseClient } from "@supabase/supabase-js";
 import type { Database } from "../supabase";
+import { normalizeName } from "$lib";
 
 /**
  * Generates a unique concert ID based on the date and whether it is private or public.
  * @param supabase Supabase client instance
- * @param data Object containing the ISO timestamp and whether the concert is private or public.
- *             If private, venueName is optional; if public, venueName is required.
- * @returns A unique concert ID in the format: `YYYY-MM-DD-I-[private|public]-[venueName]`
+ * @param isoTimestamp ISO timestamp of the concert
+ * @returns A unique concert ID in the format: `YYYY-MM-DD-I` (I=index of the concert on that date)
  */
 export async function generateConcertId(
   supabase: SupabaseClient<Database>,
-  data: { isoTimestamp: string; isPrivate?: boolean; venueName?: string },
+  isoTimestamp: string,
 ): Promise<string> {
-  const prefix = data.isPrivate ? "private" : "public";
-  let prefix2: string = "";
-  if (!data.isPrivate && data.venueName) {
-    prefix2 = data.venueName.replace(/[^a-zA-Z0-9]/g, "-").toLowerCase();
-  } else if (!data.isPrivate && !data.venueName) {
-    throw new Error("Venue name is required for public concerts");
-  }
-
-  // Schema: YYYY-MM-DD-I (I=Index)
-  const dateString = data.isoTimestamp.split("T")[0];
+  const dateString = isoTimestamp.split("T")[0];
 
   // Get amount of concerts on that date
   const { data: count, error } = await supabase.rpc("count_concerts_on_date", { date_param: dateString });
@@ -32,5 +23,5 @@ export async function generateConcertId(
   }
 
   const index = (count ?? 0) + 1;
-  return `${dateString}-${index.toString().padStart(3, "0")}-${prefix}${prefix2 ? `-${prefix2}` : ""}`;
+  return `${dateString}-${index.toString()}`;
 }
