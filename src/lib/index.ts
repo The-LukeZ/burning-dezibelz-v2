9fd4a61ea2446d56055466d970ee07c0d00f1013
The-LukeZ <luke.hent3005@gmail.com>
2025-05-31 03:46:08 +0200

    refactor: enhance buildImageUrl function to support download parameter

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -31,13 +31,14 @@ export function markdownToHtml(markdown: string, infoLink = false): string {
   return html;
 }
 
-export function buildImageUrl(filename: string, params: ImageParams = {}) {
+export function buildImageUrl(filename: string, params: ImageParams & { download?: boolean } = {}) {
   const _params = new URLSearchParams();
   if (params.width) _params.set("w", params.width.toString());
   if (params.height) _params.set("h", params.height.toString());
   if (params.quality) _params.set("q", params.quality.toString());
   if (params.format) _params.set("f", params.format);
   if (params.fit) _params.set("fit", params.fit);
+  if (params.download) _params.set("download", "true");
 
   return `/images/${encodeURIComponent(filename)}${_params.size ? "?" + _params.toString() : ""}`;
 }
