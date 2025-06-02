18f31f032967d0c932ed4f44ac8ac645a052174f
The-LukeZ <luke.hent3005@gmail.com>
2025-06-02 23:16:38 +0200

    feat: convert about section and member bios to HTML format

--- src/routes/ueber-uns/+page.ts
+++ src/routes/ueber-uns/+page.ts
@@ -1,8 +1,15 @@
+import { markdownToHtml } from "$lib";
 import aboutData from "$lib/data/about.json";
 
 export async function load() {
   return {
-    about: aboutData.about,
-    members: aboutData.members,
+    about: aboutData.about.map((s) => markdownToHtml(s)),
+    members: aboutData.members.map((m) => ({
+      ...m,
+      bio: m.bio.map((s) => markdownToHtml(s)),
+    })),
+  } as {
+    about: string[];
+    members: BandMember[];
   };
 }
