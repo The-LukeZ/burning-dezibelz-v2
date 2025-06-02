cad2fff9d14f051cebd845d37378527f83ef063b
The-LukeZ <luke.hent3005@gmail.com>
2025-06-02 23:33:39 +0200

    feat: import EventMetadata in SelectVenue component for enhanced venue data handling

--- src/lib/components/SelectVenue.svelte
+++ src/lib/components/SelectVenue.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { EventStore } from "$lib/stores/events.svelte";
+  import { EventMetadata, EventStore } from "$lib/stores/events.svelte";
   import type { Snippet } from "svelte";
   import type { ClassValue } from "svelte/elements";
   import { scale } from "svelte/transition";
