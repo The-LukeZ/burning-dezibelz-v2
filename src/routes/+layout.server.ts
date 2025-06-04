04c41de626f5eacdf0c377c129c12c517b3da635
The-LukeZ <luke.hent3005@gmail.com>
2025-06-04 16:27:44 +0200

    refactor: move admin check logic to main layout for better code organization

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,7 +3,21 @@ export const ssr = true; // Enable server-side rendering
 
 export async function load({ locals, cookies }) {
   const { session, user } = await locals.safeGetSession();
+
+  let isAdmin = false;
+  if (session && user) {
+    // Check if user is admin
+    const { data: users } = await locals.supabase.from("allowed_users").select("*");
+    const allowedUser = users?.find((u) => u.email === user.email) ?? null;
+    isAdmin = allowedUser?.role === "admin";
+
+    console.debug(
+      `User ${user.user_metadata.full_name} is authorized as ${allowedUser?.role} for admin routes`,
+    );
+  }
+
   return {
+    isAdmin: isAdmin,
     session: session,
     user: user,
     cookies: cookies.getAll(),
