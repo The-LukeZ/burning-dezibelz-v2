e2d1d0162671c25f4b1351296541e772829884f8
The-LukeZ <luke.hent3005@gmail.com>
2025-06-04 13:44:10 +0200

    refactor: restructure svelte.config.js for improved readability

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,16 @@
 import adapter from "@sveltejs/adapter-node";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import * as child_process from "node:child_process";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
-  kit: { adapter: adapter(), csrf: { checkOrigin: true }, version: { name: "2.0.0", pollInterval: 10_000 }, env: { dir: "env" } },
+  kit: {
+    adapter: adapter(),
+    csrf: { checkOrigin: true },
+    version: { name: child_process.execSync("npm pkg get version").toString().trim(), pollInterval: 10_000 },
+    env: { dir: "env" },
+  },
 };
 
 export default config;
