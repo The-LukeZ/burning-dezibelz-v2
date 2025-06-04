f11acef5a49af852b0cc15d25f129a7f912c789d
The-LukeZ <luke.hent3005@gmail.com>
2025-06-04 12:19:24 +0200

    fix: update logging message in authed layout loader

--- src/routes/dash/(authed)/+layout.server.ts
+++ src/routes/dash/(authed)/+layout.server.ts
@@ -1,7 +1,7 @@
 import { redirect } from "@sveltejs/kit";
 
 export async function load({ locals, url }) {
-  console.log("Loading authed layout...");
+  console.log("Loading authed layout..."); // This isn't logging???
   if (!locals.user || !locals.session) {
     redirect(303, "/dash/login?next=" + url.pathname);
   }
