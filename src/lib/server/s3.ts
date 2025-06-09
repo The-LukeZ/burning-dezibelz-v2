21fe42118d4fc7a1cdf9becc3a686077b2b577ef
The-LukeZ <luke.hent3005@gmail.com>
2025-06-09 11:43:50 +0200

    feat: enable forcePathStyle for S3Client configuration

--- src/lib/server/s3.ts
+++ src/lib/server/s3.ts
@@ -4,6 +4,7 @@ import { S3Client } from "@aws-sdk/client-s3";
 const s3Client = new S3Client({
   region: "auto",
   endpoint: env.R2_ENDPOINT,
+  forcePathStyle: true,
   credentials: {
     accessKeyId: env.R2_ACCESS_KEY_ID,
     secretAccessKey: env.R2_SECRET_ACCESS_KEY,
